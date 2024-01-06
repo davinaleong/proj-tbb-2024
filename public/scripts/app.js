a14b9bd6f10ecc93361248898c9659291a634a9f
Davina Leong <davina.leong.sy@gmail.com>
2024-01-06 16:15:59 +0800

    Added menu script

---
+++ public/scripts/app.js
@@ -0,0 +1,72 @@
+console.log(`file: app.js`)
+
+const DATA_ELEMENT_ATTR = `data-element`
+const DATA_ACTIVE_ATTR = `data-active`
+
+function main() {
+  console.log(`fn: main()`)
+
+  toggleMenu()
+}
+
+function toggleMenu() {
+  console.log(`fn: toggleMenu()`)
+
+  const primaryHeaderEl = getElement(`primary-header`)
+
+  if (!primaryHeaderEl) return
+
+  primaryHeaderEl.removeAttribute(DATA_ACTIVE_ATTR)
+
+  const btnMenuEl = getElement(`primary-header__btn-menu`, primaryHeaderEl)
+  if (!btnMenuEl) return
+  btnMenuEl.addEventListener(`click`, function (event) {
+    event.preventDefault()
+    primaryHeaderEl.setAttribute(DATA_ACTIVE_ATTR, `true`)
+  })
+
+  const btnCloseEl = getElement(`primary-header__btn-close`, primaryHeaderEl)
+  if (!btnCloseEl) return
+  btnCloseEl.addEventListener(`click`, function (event) {
+    event.preventDefault()
+    primaryHeaderEl.removeAttribute(DATA_ACTIVE_ATTR)
+  })
+}
+
+function getElement(key = ``, parent = null) {
+  console.log(`fn: getElement()`, { key, parent })
+
+  if (parent) {
+    try {
+      return parent.querySelector(`[${DATA_ELEMENT_ATTR}="${key}"]`)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  try {
+    return document.querySelector(`[${DATA_ELEMENT_ATTR}="${key}"]`)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+function getElements(key = ``, parent = null) {
+  console.log(`fn: getElements()`, { key, parent })
+
+  if (parent) {
+    try {
+      return parent.querySelectorAll(`[${DATA_ELEMENT_ATTR}="${key}"]`)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  try {
+    return document.querySelectorAll(`[${DATA_ELEMENT_ATTR}="${key}"]`)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+document.addEventListener("DOMContentLoaded", main)
