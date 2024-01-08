9831a198910cc6e54a3b2721645a74632cf1b39a
Davina Leong <davina.leong.sy@gmail.com>
2024-01-08 20:26:24 +0800

    Subscribe form done

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,13 @@ console.log(`file: app.js`)
 const DATA_ELEMENT_ATTR = `data-element`
 const DATA_ACTIVE_ATTR = `data-active`
 
-function main() {
+const API_URL = `https://proj-davinas-cms.test/api/`
+
+async function main() {
   console.log(`fn: main()`)
 
   toggleMenu()
+  subscribeFormHandler()
 }
 
 function toggleMenu() {
@@ -67,6 +70,54 @@ function getElements(key = ``, parent = null) {
   } catch (error) {
     console.error(error)
   }
+}
+
+async function subscribeFormHandler() {
+  console.log(`fn: subscribeFormHandler()`)
+
+  const formEl = getElement(`subscribe-form`)
+  if (!formEl) return
+
+  formEl.querySelector(`.form__help`).remove()
+  formEl.addEventListener(`submit`, async function (event) {
+    event.preventDefault()
+
+    const formData = new FormData(formEl)
+    if (formData.get("last_name")) return
+
+    const body = {
+      email: formData.get(`email`),
+    }
+
+    try {
+      const response = await fetch(
+        `https://proj-davinas-cms.test/api/blog/subscribers`,
+        {
+          method: `POST`,
+          headers: {
+            "Content-Type": `application/json`,
+            Accept: `application/json`,
+          },
+          body: JSON.stringify(body),
+        }
+      )
+
+      const { status, message } = await response.json()
+      const pEl = document.createElement(`p`)
+      pEl.classList.add(`form__help`)
+      if (status === `SUCCESS`) {
+        pEl.classList.add(`text-tbbGreen-700`)
+      } else {
+        pEl.classList.add(`text-tbbPink-700`)
+      }
+      pEl.textContent = message
+      formEl.appendChild(pEl)
+
+      formEl.reset()
+    } catch (error) {
+      console.error(`subscribeFormHandler error`, error)
+    }
+  })
 }
 
 document.addEventListener("DOMContentLoaded", main)
